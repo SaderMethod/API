6341b1efa05fc39a5105fb47a2b4efb24372cd2c
jason <jason.kilpatrick@ucd.ie>
2016-09-21 18:28:50 +0100

    Python v0.10

---
+++ 1_1/Python/Sader_GCI_demo.py
@@ -0,0 +1,59 @@
+import requests
+import xml.etree.ElementTree as etree
+
+def SaderGCI_GetLeverList( UserName, Password ):
+    payload = '''<?xml version="1.0" encoding="UTF-8" ?>
+    <saderrequest>
+      <username>'''+UserName+'''</username>
+      <password>'''+Password+'''</password>
+      <operation>LIST</operation>
+    </saderrequest>'''
+    headers = {'user-agent': 'Python API/0.1'}
+    r = requests.post('https://test.sadermethod.corpita.net/api.php', data=payload, headers=headers)
+    doc = etree.fromstring(r.content)
+
+    cantilever_ids = doc.findall('./cantilevers/cantilever/id')
+    cantilever_labels = doc.findall('./cantilevers/cantilever/label')
+
+    for a in range(len(cantilever_ids)):
+        print (cantilever_labels[a].text,cantilever_ids[a].text.replace('data_','(')+')')
+
+def SaderGCI_CalculateK( UserName, Password, LeverNumber, Frequency, QFactor ):
+    payload = '''<?xml version="1.0" encoding="UTF-8" ?>
+    <saderrequest>
+        <username>'''+UserName+'''</username>
+        <password>'''+Password+'''</password>
+        <operation>UPLOAD</operation>
+        <cantilever>
+            <id>data_'''+str(LeverNumber)+'''</id>
+            <frequency>'''+str(Frequency)+'''</frequency>
+            <quality>'''+str(QFactor)+'''</quality>
+        </cantilever>
+    </saderrequest>'''
+    headers = {'user-agent': 'Python API/0.1'}
+    r = requests.post('https://test.sadermethod.corpita.net/api.php', data=payload, headers=headers)
+    print (r.text)
+    doc = etree.fromstring(r.content)
+    if (doc.find('./status/code').text == 'OK'):
+        print ("Sader GCI Spring Constant = "+doc.find('./cantilever/k_sader').text+', 95% C.I. Error = '+doc.find('./cantilever/percent').text+'% from '+doc.find('./cantilever/samples').text+' samples.')
+
+def SaderGCI_CalculateAndUploadK( UserName, Password, LeverNumber, Frequency, QFactor, SpringK ):
+    payload = '''<?xml version="1.0" encoding="UTF-8" ?>
+    <saderrequest>
+        <username>'''+UserName+'''</username>
+        <password>'''+Password+'''</password>
+        <operation>UPLOAD</operation>
+        <cantilever>
+            <id>data_'''+str(LeverNumber)+'''</id>
+            <frequency>'''+str(Frequency)+'''</frequency>
+            <quality>'''+str(QFactor)+'''</quality>
+            <constant>'''+str(SpringK)+'''</constant>
+            <comment>Python API v0.1</comment>
+        </cantilever>
+    </saderrequest>'''
+    headers = {'user-agent': 'Python API/0.1'}
+    r = requests.post('https://test.sadermethod.corpita.net/api.php', data=payload, headers=headers)
+    print (r.text)
+    doc = etree.fromstring(r.content)
+    if (doc.find('./status/code').text == 'OK'):
+        print ("Sader GCI Spring Constant = "+doc.find('./cantilever/k_sader').text+', 95% C.I. Error = '+doc.find('./cantilever/percent').text+'% from '+doc.find('./cantilever/samples').text+' samples.')

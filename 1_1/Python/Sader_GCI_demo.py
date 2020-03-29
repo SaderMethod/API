68ddd658cfcc1a61c5437287df14cf560e602ac1
kilpatrj <kilpatrj@gmail.com>
2020-03-29 16:33:53 +0100

    Update Sader_GCI_demo.py

--- 1_1/Python/Sader_GCI_demo.py
+++ 1_1/Python/Sader_GCI_demo.py
@@ -4,7 +4,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2016 John Sader and Jason Kilpatrick
+# Copyright (c) 2016 - 2020 John Sader and Jason Kilpatrick
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
